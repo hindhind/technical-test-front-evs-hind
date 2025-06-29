ffa1a266b1e49251d3afe9af215797de01b0ee4f
Hind Chaabi <hind.chaabi@mac.home>
2025-06-30 00:55:08 +0200

    test(create-exam): clean unit tests and fix providers for dialog and usecase

--- frontend/src/app/create-exam/create-exam.component.spec.ts
+++ frontend/src/app/create-exam/create-exam.component.spec.ts
@@ -5,39 +5,87 @@ import { ExamUseCase } from '../../core/usecase/exam.usecase';
 import { ExamRepository } from '../../core/repository/exam.repository';
 import { of } from 'rxjs';
 import { MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { provideHttpClient } from '@angular/common/http';
 
 describe('CreateExamComponent', () => {
   let component: CreateExamComponent;
   let fixture: ComponentFixture<CreateExamComponent>;
+  let examUseCaseSpy: jasmine.SpyObj<ExamUseCase>;
+  let mockDialogRef: jasmine.SpyObj<MatDialogRef<CreateExamComponent>>;
 
   beforeEach(async () => {
+    const spyExamUseCase = jasmine.createSpyObj('ExamUseCase', [
+      'createNewExam',
+    ]);
+    spyExamUseCase.createNewExam.and.returnValue(of({}));
+
+    mockDialogRef = jasmine.createSpyObj('MatDialogRef', ['close']);
+
     await TestBed.configureTestingModule({
-      imports: [CreateExamComponent],
-       providers: [
-              ExamUseCase,
-              {
-                provide: ExamRepository,
-                useValue: {
-                  listOfExams: () => of([]),
-                  createExam: () => of({})
-                }
-              },
-              {
-                provide: MatDialogRef,
-                useValue: {
-                  close: jasmine.createSpy('close')
-                }
-              }
-            ]
-    })
-    .compileComponents();
+      imports: [CreateExamComponent, MatSnackBarModule],
+      providers: [
+        { provide: ExamUseCase, useValue: spyExamUseCase },
+        { provide: MatDialogRef, useValue: mockDialogRef },
+        provideHttpClient(),
+        {
+          provide: ExamRepository,
+        },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(CreateExamComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    examUseCaseSpy = TestBed.inject(ExamUseCase) as jasmine.SpyObj<ExamUseCase>;
   });
 
   it('should create', () => {
+    fixture.detectChanges();
     expect(component).toBeTruthy();
+  });
+
+  it('should have invalid form initially', () => {
+    fixture.detectChanges();
+    expect(component.examForm.invalid).toBeTrue();
+  });
+
+  it('should validate form', () => {
+    fixture.detectChanges();
+    component.examForm.setValue({
+      name: 'ana',
+      location: 'paris',
+      date: new Date(),
+      time: '12:00',
+      status: 'confirmed',
+    });
+    expect(component.examForm.valid).toBeTrue();
+  });
+
+  it('should call  createNewExam on submit', () => {
+    fixture.detectChanges();
+    component.examForm.setValue({
+      name: 'ana',
+      location: 'paris',
+      date: new Date(),
+      time: '12:00',
+      status: 'confirmed',
+    });
+    component.onSubmit();
+    expect(examUseCaseSpy.createNewExam).toHaveBeenCalled();
+  });
+
+  it('should close dialog on success', async () => {
+    fixture.detectChanges();
+    component.examForm.setValue({
+      name: 'Ana',
+      location: 'paris',
+      date: new Date(),
+      time: '12:00',
+      status: 'confirmed',
+    });
+    component.onSubmit();
+    fixture.detectChanges();
+    await fixture.whenStable();
+    expect(mockDialogRef.close).toHaveBeenCalledWith(true);
   });
 });
