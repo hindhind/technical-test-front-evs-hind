2f44ef385d750b1379e388664835f51bf91a24a8
Hind Chaabi <hind.chaabi@mac.home>
2025-06-30 01:48:28 +0200

    test(exam): refactor tests with FakeExamUseCase and add spinner, dialog, ngOnInit checks

--- frontend/src/app/exam/exam.component.spec.ts
+++ frontend/src/app/exam/exam.component.spec.ts
@@ -2,25 +2,35 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ExamComponent } from './exam.component';
 import { ExamUseCase } from '../../core/usecase/exam.usecase';
-import { ExamRepository } from '../../core/repository/exam.repository';
 import { of } from 'rxjs';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { provideHttpClient } from '@angular/common/http';
+import { signal, Signal } from '@angular/core';
+import { ExamEntity } from '../../core/entities/exam.entity';
+class FakeExamUseCase {
+  exams = signal<ExamEntity[]>([]);
+  loadExams = jasmine.createSpy('loadExams');
+}
+
 
 describe('ExamComponent', () => {
   let component: ExamComponent;
   let fixture: ComponentFixture<ExamComponent>;
+  let fakeExamUseCase: FakeExamUseCase;
+  let dialogSpy: jasmine.SpyObj<MatDialog>;
 
   beforeEach(async () => {
+    fakeExamUseCase = new FakeExamUseCase();
+
+    dialogSpy = jasmine.createSpyObj('MatDialog', ['open']);
     await TestBed.configureTestingModule({
-      imports: [ExamComponent],
+   
+
+      imports: [ExamComponent, MatDialogModule],
       providers: [
-        ExamUseCase,
-        {
-          provide: ExamRepository,
-          useValue: {
-            listOfExams: () => of([]),
-            createExam: () => of({})
-          }
-        }
+        { provide: ExamUseCase, useValue: fakeExamUseCase },
+        { provide: MatDialog, useValue: dialogSpy },
+        provideHttpClient(),
       ]
      
     })
@@ -34,5 +44,23 @@ describe('ExamComponent', () => {
   it('should create', () => {
     fixture.detectChanges();
     expect(component).toBeTruthy();
+  });
+
+
+  it('should call loadExams on ngOnInit', () => {
+    component.ngOnInit();
+    expect(fakeExamUseCase.loadExams).toHaveBeenCalled();
+  });
+
+  it('should open dialog when openNewExamModal is called', () => {
+    component.openNewExamModal();
+    expect(dialogSpy.open).toHaveBeenCalled();
+  });
+
+  it('should render a mat-spinner if no exams are present', () => {
+    fakeExamUseCase.exams.set([]);
+    fixture.detectChanges();
+    const spinner = fixture.nativeElement.querySelector('mat-spinner');
+    expect(spinner).toBeTruthy();
   });
 });
