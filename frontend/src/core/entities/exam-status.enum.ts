0026d1e9bf57b21cd2c1bf3efea8784476f2d342
Hind Chaabi <hind.chaabi@mac.home>
2025-06-29 23:44:31 +0200

    refactor(exam): remove exam-status enum and update entity structure

--- frontend/src/core/entities/exam-status.enum.ts
+++
@@ -1,7 +0,0 @@
-export enum ExamStatus {
-    Confirmed = 'confirmed',
-    Pending = 'pending',
-    ToOrganize = 'to-organize',
-    Cancelled = 'cancelled',
-    Searching = 'searching'
-  }
--- frontend/src/core/entities/exam.entity.ts
+++ frontend/src/core/entities/exam.entity.ts
@@ -1,10 +1,7 @@
-import { ExamStatus } from "./exam-status.enum";
-
 export interface ExamEntity {
-    id: number;
     candidateName: string;
     location: string;
     date: string; 
     time: string;
-    status: ExamStatus;
+    status: string;
 }
